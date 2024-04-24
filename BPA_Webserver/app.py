71d3402d824851c0a8c6729a6ba7707650c80cc4
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-04-24 11:13:54 +0200

    update

--- BPA_Webserver/app.py
+++ BPA_Webserver/app.py
@@ -1,10 +1,28 @@
-from website import create_app
+from flask import Flask
+import threading
+import time
+import sys
+sys.path.append('../')
+from src.subscriber import OPCUASubscriber
 
-app = create_app()
 
+app = Flask(__name__)
+ 
+subscriber = OPCUASubscriber(server_url="opc.tcp://uademo.prosysopc.com:53530/OPCUA/SimulationServer", 
+                              node_id="ns=6;s=MySwitch")
+ 
+test_thread = threading.Thread(target=subscriber.run)
+ 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return "Hallo, Flask läuft!"
+ 
+# Die Funktion zum Starten der Flask-App und des Threads
+def start_app():
+    # Thread starten
+    test_thread.start()
+    # Flask-Server starten
+    app.run(debug=True, use_reloader=False)  # use_reloader=False ist wichtig, um Konflikte mit dem Thread zu vermeiden
+ 
 if __name__ == '__main__':
-    app.run(port=4000, debug=True)
+    start_app()
