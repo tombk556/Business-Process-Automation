4ac8e49ca1b342180807a0e2d74ba95d5ba1ed1a
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-05-19 14:06:17 +0200

    added loggin

--- BPA_Webserver/src/utils.py
+++ BPA_Webserver/src/utils.py
@@ -1,11 +1,15 @@
+from config import settings
 from opcua import Client
 import requests
 import time
 import sys
 import threading
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:: %(message)s')
+logger = logging.getLogger(__name__)
 
 sys.path.append('../')
-from config import settings
 
 # Retrieve settings
 opcua_url = settings.opcua_url
@@ -15,13 +19,14 @@ trigger_url = settings.aas_url
 latest_auto_id_lock = threading.Lock()
 latest_auto_id = None
 
+
 class SubHandler(object):
     def datachange_notification(self, node, val, data):
         global latest_auto_id
         with latest_auto_id_lock:
             latest_auto_id = val
-        print(f"New data change event: {val}")
         trigger_action_based_on_auto_id(val)
+
 
 def trigger_action_based_on_auto_id(auto_id):
     try:
@@ -29,13 +34,16 @@ def trigger_action_based_on_auto_id(auto_id):
         if response.status_code == 200:
             href = search_id_short_and_href(response.json(), auto_id)
             if href:
-                print(f"Found href: {href}")
+                logger.info(f"Href found in AAS Shell: {href} for Auto ID: {auto_id}")
             else:
-                print(f"Failed to find href for Auto ID: {auto_id}")
+                logger.warning(f"Failed to get href for auto_id {auto_id} from AAS shell")
         else:
-            print(f"Failed to trigger action for Auto ID: {auto_id}, status code: {response.status_code}")
+            logger.error(
+                f"Failed to trigger action for Auto ID: {auto_id}, status code: {response.status_code}")
     except Exception as e:
-        print(f"Error triggering action for Auto ID: {auto_id}, error: {e}")
+        logger.error(
+            f"Error triggering action for Auto ID: {auto_id}, error: {e}")
+
 
 def search_id_short_and_href(data, target_id_short):
     if isinstance(data, dict):
@@ -55,38 +63,32 @@ def search_id_short_and_href(data, target_id_short):
                 return result
     return None
 
+
 def opcua_subscriber():
     global latest_auto_id
     client = Client(opcua_url)
-    
+
     try:
         client.connect()
-        print("Connected to OPC UA server")
-        
-        # Get the objects node
+        logger.info("Connected to OPC UA server")
+
         objects = client.get_objects_node()
-        
-        # Get the AutoID variable node
+
         auto_id_obj = objects.get_child(["2:AutoID"])
         auto_id_node = auto_id_obj.get_child(["2:AutoID"])
-        
-        # Create a subscription handler
+
         handler = SubHandler()
         sub = client.create_subscription(100, handler)
-        
-        # Subscribe to the AutoID variable node
+
         sub.subscribe_data_change(auto_id_node)
-        
-        # Keep the client running to receive data change events
+
         while True:
             time.sleep(1)
-    
+
     except Exception as e:
-        print(f"An error occurred: {e}")
-    
+        logger.exception(f"Error subscribing to OPC UA server, error: {e}")
+
     finally:
         client.disconnect()
-        print("Disconnected from OPC UA server")
+        logger.info("Disconnected from OPC UA server")
 
-if __name__ == '__main__':
-    opcua_subscriber()
