79c65782e1992516d367b818d06be058bdb0b645
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-05-21 19:34:45 +0200

    added mongodb data fetching and displaying in html

--- BPA_Webserver/website/views.py
+++ BPA_Webserver/website/views.py
@@ -1,7 +1,16 @@
 from flask import render_template, Blueprint
-
+from pymongo import MongoClient
+import sys
+sys.path.append('../')
+from config import settings
 views = Blueprint('views', __name__)
 
+client = MongoClient(settings.mongodb_url)
+db = client["BPA_DB"]
+collection = db["InspectionData"]
+
 @views.route('/')
-def home():
-    return render_template("home.html") 
+def index():
+    data = list(collection.find({})) 
+    print(data)
+    return render_template('home.html', data=data)
