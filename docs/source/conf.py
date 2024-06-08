05a0f06f6326f140dffe83e2bcecf44fd8b8ad17
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-06-08 10:55:42 +0200

    added sphinx doc

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../../'))
+
+project = 'BPA SS24 Code Dokumentation'
+copyright = '2024, Tom Bischopink, Konrad Adamski'
+author = 'Tom Bischopink, Konrad Adamski'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
---
+++ docs/source/src.rst
@@ -0,0 +1,29 @@
+src package
+===========
+
+Submodules
+----------
+
+src.MQTT\_Camera module
+-----------------------
+
+.. automodule:: src.MQTT_Camera
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+src.OPC\_UA\_Subscriber\_AssemplyLine module
+--------------------------------------------
+
+.. automodule:: src.OPC_UA_Subscriber_AssemplyLine
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: src
+   :members:
+   :undoc-members:
+   :show-inheritance:
