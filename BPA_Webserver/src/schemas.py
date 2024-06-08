af702994f86f8393d6346724a6bd3f10887c5af0
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-06-08 10:17:50 +0200

    deleted most of the stuff lol

--- BPA_Webserver/src/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-
-class InspectionInstance(BaseModel):
-    auto_id: str
-    ip: str
-    href: str
-    timestamp: datetime = Field(default_factory=datetime.now)
